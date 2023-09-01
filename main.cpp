53bf391cd7889e6682db707aee8550c1e07a54af
rmendoza42 <rmendoza42@students.avc.edu>
2023-09-01 10:19:06 -0700

    2-1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int miles = 4276;
+    int balance = -20;
+    int days = 18900;
+
+    cout << "We have made a long journey of " << miles << " miles.\n";
+    cout << "Our checking account balance is " << balance << ".\n";
+    cout << "About " << days << " days to ago Columbus stood on this spot.\n";
+    return 0;
+}
